2e885b98d280724b80487c85d222a7fcca8e693b
Dima Suhinin <suhinin.dm@gmail.ru>
2016-02-06 11:38:46 +0300

    module4-task1

---
+++ js/pictures.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/* global pictures : true */
+(function() {
+
+  var container = document.querySelector('.pictures');
+  var filters = document.querySelector('.filters');
+
+  filters.classList.add('hidden');
+
+  pictures.forEach(function(picture) {
+    var elem = getPicturesFromTemplate(picture);
+    container.appendChild(elem);
+  });
+
+  filters.classList.remove('hidden');
+
+  function getPicturesFromTemplate(data) {
+    var template = document.querySelector('#picture-template');
+    var pictureElement;
+
+    if ('content' in template) {
+      pictureElement = template.content.children[0].cloneNode(true);
+    } else {
+      pictureElement = template.children[0].cloneNode(true);
+    }
+
+    pictureElement.querySelector('.picture-likes').textContent = data.likes;
+    pictureElement.querySelector('.picture-comments').textContent = data.comments;
+
+    var image = new Image('182', '182');
+
+    var replaceImg = pictureElement.querySelector('img');
+    var imageLoadTimeout;
+    var IMAGE_TIMEOUT = 10000;
+
+    image.src = data.url;
+
+    image.onload = function() {
+      clearTimeout(imageLoadTimeout);
+      pictureElement.replaceChild(image, replaceImg);
+    };
+
+    image.onerror = function() {
+      pictureElement.classList.add('picture-load-failure');
+    };
+
+    imageLoadTimeout = setTimeout(function() {
+      image.src = '';
+      pictureElement.classList.add('picture-load-failure');
+    }, IMAGE_TIMEOUT);
+
+    return pictureElement;
+  }
+
+})();
